143ae4db0be7ffb9d9cb969aaa7ac03425bea050
HaglundA <ah3918@ic.ac.uk>
2025-06-02 10:30:56 +0000

    restructure

--- test_run.sh
+++
@@ -1,5 +0,0 @@
-export NXF_LOG_FILE=/var/lib/docker/alex_tmp/NF_WORK/nextflow.log
-workdir=/var/lib/docker/alex_tmp/NF_WORK
-
-nextflow run main.nf \
-    --gwas_csv test/inputs/gwas_list.csv -w ${workdir} 
